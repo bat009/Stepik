41265e86492aee1180789620035fc4a2d7de344c
Albert Glushkov <bat009@yandex.ru>
2017-09-24 19:49:18 +0500

    Added classes: Frontend, Main, PageGenerator and other things

---
+++ src/main/java/com/soul/servlets/Frontend.java
@@ -0,0 +1,44 @@
+package com.soul.servlets;
+
+import com.soul.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Frontend extends HttpServlet{
+    private String login = "";
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> map = new HashMap<>();
+        map.put("param", req.getParameterMap().toString());
+
+        resp.getWriter().println(PageGenerator.getInstance().getPage("page.html",map));
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("param",req.getParameterMap().toString());
+
+        String message = req.getParameter("message");
+
+        resp.setContentType("text/html;charset=utf-8");
+
+        if (message == null || message.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+        pageVariables.put("message", message == null ? "" : message);
+
+        resp.getWriter().println(PageGenerator.getInstance().getPage("page.html", pageVariables));
+
+    }
+}
